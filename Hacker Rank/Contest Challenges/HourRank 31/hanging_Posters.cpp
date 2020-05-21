6ac841c3bf5d6649c604187fcc6d9fb023321bb2
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-05-21 18:56:43 +0530

    1

---
+++ Hacker Rank/Contest Challenges/HourRank 31/hanging_Posters.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include<algorithm>
+#include<math.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+
+     ll num_Poster,height_Author;
+     cin>>num_Poster>>height_Author;
+
+     ll wall_point[num_Poster],len_Poster[num_Poster];
+
+     for(ll i = 0; i < num_Poster; i++)
+     {
+         cin>>wall_point[i];
+     }    
+     
+     for(ll i = 0; i < num_Poster; i++)
+     {
+         cin>>len_Poster[i];
+     }    
+     
+     ll max_lad = 0;
+    for(ll i = 0;i < num_Poster;i++){
+
+        double l = (wall_point[i]-height_Author)-(double)len_Poster[i]*0.25;
+        
+        int ans = ceil(l);
+        if(ans > max_lad){
+            max_lad = ans;
+        }
+    }
+    cout<<max_lad;
+}
