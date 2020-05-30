b154e7305bc0cd3605ccd194a63ffd6de3fd7618
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-05-30 17:14:23 +0530

    1

---
+++ CodeChef/Practices/DSA- Learning Series/1) Complexity Analysis + Basics Warm Up/1) Life, the Universe, and Everything.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    while(cin>>x)
+    {
+        if(x==42)
+        {
+            break;
+        }
+        cout<<x<<endl;
+    }
+
+}
