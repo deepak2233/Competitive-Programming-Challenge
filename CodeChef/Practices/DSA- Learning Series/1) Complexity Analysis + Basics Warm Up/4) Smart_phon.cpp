9fdf3dc94d9c164aa962109d1abe04a138eafed5
Deepak yadav <44837205+deepak2233@users.noreply.github.com>
2020-05-31 20:00:08 +0530

    1

---
+++ CodeChef/Practices/DSA- Learning Series/1) Complexity Analysis + Basics Warm Up/4) Smart_phon.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#define vi vector<int>
+using namespace std;
+
+int main(void)
+{
+
+    long long n, tmp; cin >> n;
+    vi a;
+    for(long long i = 0; i < n;i++)
+    {
+      cin >> tmp; a.push_back(tmp);
+    }
+    sort(a.begin(), a.end());
+    reverse(a.begin(), a.end());
+    long long maximum = 0;
+    for(long long i = 0; i < n; i++)
+    {
+       maximum = max(maximum, a[i] * (i+1));
+    }
+    cout << maximum << endl;
+    return 0;
+}
